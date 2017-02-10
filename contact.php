6452ac4cf437c3f26ccd5037017baffa7e625003
Deep Datta <dattausc@gmail.com>
2017-02-10 15:35:39 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,26 @@
-<?php 
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $message = $_POST['message'];
-      $formcontent="From: $name \n Message: $message";
-      $recipient = "dattausc@gmail.com";
-      $subject = "Contact Form";
-      $mailheader = "From: $email \r\n";
-      mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-      echo "Thank You!";
+<?php
+if (isset($_POST['name'],$_POST['email'],$_POST['message'])){
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$from = 'From: ' . $_POST['email']; 
+$to  = 'dattausc@gmail.com'; 
+$subject = 'subject';
+
+
+$body = "From: $name\n E-Mail: $email\nMessage:\n $message";
+if ($name != '' && $email != '' && $message != '') {
+
+        if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Success</p>';
+    } else { 
+        echo '<p>Error sending message</p>'; 
+    } 
+
+
+} else {
+    echo '<p>fill the form</p>';
+}
+}
 ?>
